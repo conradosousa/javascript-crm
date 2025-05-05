579c396e42ce9011c99e5d8e01e88efdcc3bcd21
conradosousa <conradojns@hotmail.com>
2025-05-05 17:00:54 -0300

    clientes

---
+++ clientes.js
@@ -0,0 +1,22 @@
+let clientes = [
+    {
+        nome: "Conrado",
+        email: "conradojns@hotmail.com",
+        telefone: "(85) 99696-6656",
+        data: "05/05/2025"
+    },
+
+    {
+        nome: "Emmanuel",
+        email: "Emmanuel@hotmail.com",
+        telefone: "(85) 99696-6652",
+        data: "05/05/2025"
+    },
+
+    {
+        nome: "Ana",
+        email: "ana@hotmail.com",
+        telefone: "(85) 99696-6655",
+        data: "05/05/2025"
+    },
+]
